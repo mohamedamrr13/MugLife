9e0e4be84e47664485332ccd1e267fc419e25862
Mohamed Amr <mohamed.egypt140@gmail.com>
2025-08-02 06:15:29 +0300

    add get categories repo and cubit

--- lib/features/home/data/models/category_model.dart
+++ lib/features/home/data/models/category_model.dart
@@ -1,8 +1,16 @@
-class CategoryModel{
- final String name;
- final String image;
+class CategoryModel {
+  final String name;
+  final String image;
 
-  CategoryModel({required this.name,required this.image});
+  CategoryModel({required this.name, required this.image});
 
+  /// Create from Firestore document (Map)
+  factory CategoryModel.fromJson(Map<String, dynamic> json) {
+    return CategoryModel(name: json['name'] ?? '', image: json['image'] ?? '');
+  }
 
+  /// Convert to Map (optional, useful for adding to Firestore)
+  Map<String, dynamic> toJson() {
+    return {'name': name, 'image': image};
+  }
 }
---
+++ lib/features/home/logic/get_categories_cubit/get_categories_cubit.dart
@@ -0,0 +1,22 @@
+import 'package:bloc/bloc.dart';
+import 'package:drinks_app/features/home/data/models/category_model.dart';
+import 'package:drinks_app/features/home/data/repos/get_categories_repo/get_categories_repo_impl.dart';
+import 'package:drinks_app/features/home/presentation/screens/widgets/category_item.dart';
+import 'package:meta/meta.dart';
+
+part 'get_categories_state.dart';
+
+class GetCategoriesCubit extends Cubit<GetCategoriesState> {
+  GetCategoriesCubit(this.getCategoriesRepoImpl)
+    : super(GetCategoriesInitial());
+  final GetCategoriesRepoImpl getCategoriesRepoImpl;
+
+  Future<void> getCategories() async {
+    emit(GetCategoriesLoading());
+    final result = await getCategoriesRepoImpl.getCategories();
+    result.fold(
+      (left) => emit(GetCategoriesFailure(left.message)),
+      (right) => emit(GetCategoriesSuccess(right)),
+    );
+  }
+}
