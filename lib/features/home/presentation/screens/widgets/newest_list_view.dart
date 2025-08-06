1198f2acc2ee4870f3739f14bad749427fe9900f
Mohamed Amr <mohamed.egypt140@gmail.com>
2025-08-06 08:51:55 +0300

    unused imports

--- lib/features/home/presentation/screens/widgets/newest_list_view.dart
+++ lib/features/home/presentation/screens/widgets/newest_list_view.dart
@@ -2,7 +2,6 @@ import 'package:drinks_app/core/routing/app_router.dart';
 import 'package:drinks_app/features/home/logic/get_featured_product_cubit/get_featured_products_cubit.dart';
 import 'package:drinks_app/features/home/presentation/screens/widgets/newest_list_view_item.dart';
 import 'package:drinks_app/features/product/data/models/product_model.dart';
-import 'package:drinks_app/utils/colors/app_colors.dart';
 import 'package:drinks_app/utils/helper/helper_functions.dart';
 import 'package:drinks_app/utils/shared/loading_data_widget.dart';
 import 'package:flutter/material.dart';
